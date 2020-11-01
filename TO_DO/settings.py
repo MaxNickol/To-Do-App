7df66b68b91720471a107bb5efdd777e3e06a058
MaxNickol <internprogr97@gmail.com>
2020-11-01 18:12:33 +0300

    Deployment

--- TO_DO/settings.py
+++ TO_DO/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -120,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+django_heroku.settings(locals())
